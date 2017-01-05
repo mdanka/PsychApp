be4ba42606a1a0a2c1eafec9739536b626907f97
Miklos Danka <danka.miklos@gmail.com>
2017-01-05 19:23:20 +0000

    backed up the typescript server and created a js one

---
+++ server/queries.js
@@ -0,0 +1,73 @@
+/**
+ * Created by mdanka on 30/12/2016.
+ */
+
+var promise = require('bluebird');
+
+var options = {
+    // Initialization Options
+    promiseLib: promise
+};
+
+var pgp = require('pg-promise')(options);
+var connectionString = 'postgres://localhost:5432/puppies';
+var db = pgp(connectionString);
+
+// add query functions
+function getAllPuppies(req, res, next) {
+    db.any('select * from pups')
+        .then(function (data) {
+            res.status(200)
+                .json({
+                    status: 'success',
+                    data: data,
+                    message: 'Retrieved ALL puppies'
+                });
+        })
+        .catch(function (err) {
+            return next(err);
+        });
+}
+
+function getSinglePuppy(req, res, next) {
+    var pupID = parseInt(req.params.id);
+    db.one('select * from pups where id = $1', pupID)
+        .then(function (data) {
+            res.status(200)
+                .json({
+                    status: 'success',
+                    data: data,
+                    message: 'Retrieved ONE puppy'
+                });
+        })
+        .catch(function (err) {
+            return next(err);
+        });
+}
+
+function createPuppy(req, res, next) {
+    req.body.age = parseInt(req.body.age);
+    db.none('insert into pups(name, breed, age, sex)' +
+        'values(${name}, ${breed}, ${age}, ${sex})',
+        req.body)
+        .then(function () {
+            res.status(200)
+                .json({
+                    status: 'success',
+                    message: 'Inserted one puppy'
+                });
+        })
+        .catch(function (err) {
+            return next(err);
+        });
+}
+
+
+
+module.exports = {
+    getAllPuppies: getAllPuppies,
+    getSinglePuppy: getSinglePuppy,
+    createPuppy: createPuppy,
+    // updatePuppy: updatePuppy,
+    // removePuppy: removePuppy
+};
