362204c7cea73124fed5ad52298bb740ab957991
Miklos Danka <danka.miklos@gmail.com>
2017-01-08 17:30:47 +0000

    Updated the database type from date to timestamp

--- server/psychapp.sql
+++ server/psychapp.sql
@@ -7,11 +7,18 @@ CREATE TABLE meditation (
   id SERIAL PRIMARY KEY,
   nickname VARCHAR,
   answer VARCHAR,
-  date DATE
+  date TIMESTAMP WITH TIME ZONE
 );
 
 INSERT INTO meditation (nickname, answer, date)
-  VALUES ('testuser', 'yes', '2000-01-01');
+  VALUES ('testuser', 'yes', '2000-01-01T00:00:00.000Z');
 
 INSERT INTO meditation (nickname, answer, date)
-  VALUES ('testuser', 'no', '2000-02-01');
+  VALUES ('testuser', 'no', '2000-01-01T00:00:00.000Z');
+
+GRANT ALL PRIVILEGES ON DATABASE psychapp TO psychapp;
+
+GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA public TO psychapp;
+
+GRANT USAGE, SELECT ON SEQUENCE meditation_id_seq TO psychapp;
+
